f18ac503e20c83f391f2938ee41fd34c1aa01238
krishna <gopikrishna.mgk7@gmail.com>
2023-03-27 20:19:48 +0530

    changed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello world! This is Gopi krishna')
+  res.send('Hello world!')
 })
 
 app.listen(3000)
